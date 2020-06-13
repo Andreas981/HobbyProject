32f999038928bea924e6cd68aab1f9bc78708b30
Andreas Mikalsen <amikal@hotmail.no>
2020-06-13 16:38:27 +0200

    Filtering on poststed

--- app/src/main/java/no/andreasmikalsen/hobbyapplication/ListFragment.java
+++ app/src/main/java/no/andreasmikalsen/hobbyapplication/ListFragment.java
@@ -1,6 +1,7 @@
 package no.andreasmikalsen.hobbyapplication;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,9 +12,14 @@ import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -24,6 +30,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import no.andreasmikalsen.hobbyapplication.Model.Workshop;
 import no.andreasmikalsen.hobbyapplication.Utility.ItemArrayAdapter;
@@ -37,6 +46,10 @@ public class ListFragment extends Fragment {
     private ListView listView;
     private ItemArrayAdapter itemArrayAdapter;
     private ArrayList<Workshop> workshops = new ArrayList<>();
+    private ArrayList<Workshop> mWorkshops = new ArrayList<>();
+    private EditText filterPostSted;
+    private Button filterBtn;
+    private Button clearFilterBtn;
 
     public ListFragment() {
         // Required empty public constructor
@@ -53,14 +66,63 @@ public class ListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        
+
         listView = view.findViewById(R.id.listView);
         itemArrayAdapter = new ItemArrayAdapter(getActivity().getApplicationContext(), R.layout.item_layout);
+        filterPostSted = view.findViewById(R.id.list_filter_poststed_edittext);
+        filterBtn = view.findViewById(R.id.list_filter_btn);
+        clearFilterBtn = view.findViewById(R.id.list_clear_filter_btn);
 
         InputStream inputStream = getResources().openRawResource(R.raw.verksted);
         getWorkshopFromFile(inputStream);
         populateListView();
 
+
+        filterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String filterPostStedText = filterPostSted.getText().toString();
+                if(filterPostStedText.trim().length() <= 0){
+                    Snackbar.make(v, "Filter tekst er tom", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }else{
+                    ArrayList<Workshop> tempList= filterWorkshops(filterPostStedText);
+                    if(tempList.size() <= 0){
+                        Snackbar.make(v, "Fant ingen verksteder med dette filteret", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }else{
+                        mWorkshops = tempList;
+                        populateListView();
+                        filterPostSted.setText("");
+                        //Hide keyboard
+                        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(filterPostSted.getWindowToken(), 0);
+                    }
+                }
+            }
+        });
+
+        clearFilterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWorkshops = workshops;
+                populateListView();
+            }
+        });
+
+    }
+
+    private ArrayList<Workshop> filterWorkshops(String value){
+        ArrayList<Workshop> secondList = new ArrayList<>();
+
+        for( int i = 0 ; i < workshops.size(); i++) {
+            if (workshops.get(i).getPoststed().toLowerCase().equals(value)) {
+                secondList.add(workshops.get(i));
+            }
+        }
+
+
+        return secondList;
     }
 
     private void getWorkshopFromFile(InputStream inputStream) {
@@ -71,11 +133,12 @@ public class ListFragment extends Fragment {
             while ((csvLine = reader.readLine()) != null) {
                 String[] row = csvLine.split(";");
                 if(test != 0){
-                    workshops.add(new Workshop(row[0], row[1], row[2], row[3]));
+                    workshops.add(new Workshop(row[0].trim(), row[1].trim(), row[2].trim(), row[3].trim()));
                 }else{
                     test = 1;
                 }
             }
+            mWorkshops = workshops;
         }
         catch (IOException ex) {
             throw new RuntimeException("Error in reading CSV file: "+ex);
@@ -92,14 +155,18 @@ public class ListFragment extends Fragment {
 
 
     private void populateListView() {
+        itemArrayAdapter.clear();
+
         Parcelable state = listView.onSaveInstanceState();
         listView.setAdapter(itemArrayAdapter);
         listView.onRestoreInstanceState(state);
 
-        for(Workshop scoreData:workshops ) {
-            itemArrayAdapter.add(scoreData);
+        for(Workshop workshop:mWorkshops ) {
+            itemArrayAdapter.add(workshop);
         }
     }
+
+
 
 
 }
--- app/src/main/java/no/andreasmikalsen/hobbyapplication/Utility/ItemArrayAdapter.java
+++ app/src/main/java/no/andreasmikalsen/hobbyapplication/Utility/ItemArrayAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -16,7 +18,8 @@ import no.andreasmikalsen.hobbyapplication.Model.Workshop;
 import no.andreasmikalsen.hobbyapplication.R;
 
 public class ItemArrayAdapter extends ArrayAdapter<Workshop> {
-    private ArrayList<Workshop> scoreList = new ArrayList();
+    private ArrayList<Workshop> workshops = new ArrayList();
+    private ArrayList<Workshop> mOriginalValues = new ArrayList();
 
     static class ItemViewHolder {
         TextView bedriftsNavn, adresse, postnummer, poststed;
@@ -28,18 +31,30 @@ public class ItemArrayAdapter extends ArrayAdapter<Workshop> {
 
     @Override
     public void add(Workshop object) {
-        scoreList.add(object);
+        workshops.add(object);
         super.add(object);
     }
 
     @Override
     public int getCount() {
-        return this.scoreList.size();
+        return this.workshops.size();
     }
 
     @Override
     public Workshop getItem(int index) {
-        return this.scoreList.get(index);
+        return this.workshops.get(index);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+        workshops.clear();
+        notifyDataSetChanged();
     }
 
     @Override
@@ -66,4 +81,57 @@ public class ItemArrayAdapter extends ArrayAdapter<Workshop> {
         viewHolder.postnummer.setText(stat.getPostnummer());
         return row;
     }
+
+    /*@Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint,FilterResults results) {
+
+                workshops = (ArrayList<Workshop>) results.values; // has the filtered values
+                notifyDataSetChanged();  // notifies the data with new filtered values
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();        // Holds the results of a filtering operation in values
+                List<Workshop> FilteredArrList = new ArrayList<Workshop>();
+
+                if (mOriginalValues == null) {
+                    mOriginalValues = new ArrayList<Workshop>(workshops); // saves the original data in mOriginalValues
+                }
+
+                /********
+                 *
+                 *  If constraint(CharSequence that is received) is null returns the mOriginalValues(Original) values
+                 *  else does the Filtering and returns FilteredArrList(Filtered)
+                 *
+                 ********/
+                /*if (constraint == null || constraint.length() == 0) {
+
+                    // set the Original result to return
+                    results.count = mOriginalValues.size();
+                    results.values = mOriginalValues;
+                } else {
+                    constraint = constraint.toString().toLowerCase();
+                    for (int i = 0; i < mOriginalValues.size(); i++) {
+                        String data = mOriginalValues.get(i);
+                        if (data.toLowerCase().startsWith(constraint.toString())) {
+                            FilteredArrList.add(data);
+                        }
+                    }
+                    // set the Filtered result to return
+                    results.count = FilteredArrList.size();
+                    results.values = FilteredArrList;
+                }
+                return results;
+            }
+        };
+        return filter;
+    }*/
+
+
+
 }
